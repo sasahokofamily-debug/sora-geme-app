0335319ca7c4e9cf84321d4ce377150a39cf042e
sasahokofamily-debug <sasahokofamily@gmail.com>
2026-08-02 09:02:07 +0900

    Bump cache for WebStore shared account redesign R97

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME="shooking-ii-v96";
-const SW_BUILD="96";
+const CACHE_NAME="shooking-ii-v97";
+const SW_BUILD="97";
 const GAME_SCRIPTS=[
-["ui-patch.js",6],["app-notice.js",8],["release-current.js",15],["firebase-config.js",2],
+["ui-patch.js",6],["app-notice.js",8],["release-current.js",16],["firebase-config.js",2],
 ["auth-session-fix.js",2],["google-login.js",11],["google-login-fix.js",3],
 ["password-reset-fix.js",4],["password-change.js",2],["login-cool.js",3],
 ["login-success-warp.js",6],["login-failure-effect.js",3],["guest-login.js",2],
